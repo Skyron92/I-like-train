a24f9fe678ad3be15319fb57b48dadc6a70675b9
Skyron92 <courtier.leo@gmail.com>
2023-03-16 16:02:46 +0000

    Poussez-vous je push !

--- Assets/Léo/Script/Player/PlayerController.cs
+++ Assets/Léo/Script/Player/PlayerController.cs
@@ -6,26 +6,29 @@ public class PlayerController : MonoBehaviour
 {
         [Header("Player settings")]
         [Tooltip("Vitesse de déplacement.")] [Range(1, 10)] [SerializeField] private float _speed;
+        [Tooltip("Vitesse de déplacement.")] [SerializeField] private float _rotationSpeed;
         [Tooltip("Gravity multiplier.")] [Range(1, 10)] [SerializeField] private float Weight;
         private CharacterController _controller;
         private float gravityValue = -9.81f;
         private Vector3 playerVelocity;
         private Vector3 _input;
-        [Header("Camera settings")]
+
+        [Header("Camera settings")] [SerializeField]
+        private Transform cameraTransform;
         [Tooltip("Sensibilité de la souris.")]
         [Range(0.1f, 9f)] [SerializeField] private float YCameraSensitivity = 2f;
         [Range(0.1f, 9f)] [SerializeField] private float XCameraSensitivity = 0.5f;
         [Tooltip("Limite de la rotation verticale de la caméra. Ne peut pas dépasser 90.")]
         [Range(0f, 360f)] [SerializeField] float yRotationLimit = 88f;
         Vector2 rotation = Vector2.zero;
-        [SerializeField] private Texture2D cursorSprite;
         public static float timeSpeed = 1;
+        private Animator _animator;
     
         private void Awake() {
             _controller = GetComponent<CharacterController>();
+            _animator = GetComponent<Animator>();
             Cursor.lockState = CursorLockMode.Confined;
             Cursor.visible = false;
-            Cursor.SetCursor(cursorSprite, new Vector2(Screen.width/2, Screen.height/2), CursorMode.ForceSoftware);
         }
     
         void Update() {
@@ -40,14 +43,18 @@ public class PlayerController : MonoBehaviour
 
         public void ReadWalk(InputAction.CallbackContext context) {
             _input = context.ReadValue<Vector2>();
-            playerVelocity = new Vector3(_input.x, 0, _input.y);
+            playerVelocity = new Vector3(0, 0, _input.y);
+            _animator.SetFloat("Vertical", playerVelocity.z);
         }
     
     
         void RotateCamera() {
-            var xQuat = Quaternion.AngleAxis(rotation.x * XCameraSensitivity * timeSpeed, Vector3.up);
-            var yQuat = Quaternion.AngleAxis(rotation.y * YCameraSensitivity * timeSpeed, Vector3.left);
-            transform.localRotation = xQuat * yQuat;
+            rotation.x += Input.GetAxis("Mouse X") * XCameraSensitivity;
+            rotation.y += Input.GetAxis("Mouse Y") * YCameraSensitivity;
+            rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
+            var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+            var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+            cameraTransform.localRotation = xQuat * yQuat;
         }
 
         public void ReadCamera(InputAction.CallbackContext context) {
